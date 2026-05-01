832c22d86e77c80904100c4c0f4e58211c4c5242
finn.paustian <wndeRkind1337>
2026-05-01 11:31:18 +0200

    # added true in cookies

--- backend/src/server.js
+++ backend/src/server.js
@@ -26,7 +26,8 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: false,
+        secure: true,
+        sameSite: 'none',
         maxAge: 7 * 24 * 60 * 60 * 1000
     }
 }))
