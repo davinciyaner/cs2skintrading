b6b7535d09ff0ee3f2cdc340ed186038b029a0ec
finn.paustian <wndeRkind1337>
2026-05-01 13:24:39 +0200

    # steam umgehen..

--- src/app/listings/page.js
+++ src/app/listings/page.js
@@ -1,6 +1,6 @@
 'use client'
 import { useState, useEffect } from 'react'
-import { api } from '../../lib/api'
+import {api} from "@/app/lib/api";
 
 // Steam API Key Info Modal
 function ApiKeyModal({ onSave, onClose }) {
