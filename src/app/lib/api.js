8b21e9ce1c7dad31ab9206ec5ac5f5afba464703
finn.paustian <wndeRkind1337>
2026-05-04 16:13:15 +0200

    # produktion api

--- src/app/lib/api.js
+++ src/app/lib/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL
+const API_URL = process.env.API_URL
 
 async function apiFetch(path, options = {}) {
     const res = await fetch(`${API_URL}${path}`, {
